4b1ab84abfce28483875e4880eea220b713e73b7
rmad17 <souravbasu17@gmail.com>
2016-09-22 15:55:25 +0530

    Created django project

---
+++ calsync/local_settings_sample.py
@@ -0,0 +1,17 @@
+SECRET_KEY = ''
+
+DEBUG = True
+
+DATABASES = {
+   'default': {
+       'ENGINE': '',
+       'NAME': '',
+       'USER': '',
+       'PASSWORD': '',
+       'HOST': '',
+       'PORT': '',
+       'TEST': {
+           'NAME': '',
+       },
+   }
+}
