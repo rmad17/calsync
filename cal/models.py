b9871c6df427aff043e69cbbd94770b97918b0d5
rmad17 <souravbasu17@gmail.com>
2016-09-24 12:43:40 +0530

    Added code for database entry

--- cal/models.py
+++ cal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Events(models.Model):
+    user = models.ForeignKey(User)
+    description = models.CharField(max_length=500, unique=True)
+    updated = models.DateTimeField(auto_now=False, auto_now_add=False)
--- cal/views.py
+++ cal/views.py
@@ -1,11 +1,14 @@
+import httplib2
+from datetime import datetime
 from django.shortcuts import render, reverse, redirect
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
 
-import httplib2
-from datetime import datetime
 from apiclient.discovery import build
 from oauth2client.client import AccessTokenCredentials
+
+from .models import Events
 # Create your views here.
 
 
@@ -17,6 +20,8 @@ def home(request):
         service = connect_helper(request.user)
         eventsResult = service.events().list(calendarId='primary').execute()
         events = eventsResult.get('items', [])
+        for event in events:
+            save_event(event, request.user)
         context['events'] = events
     # import pdb; pdb.set_trace()
     return render(request, 'index.html', context)
@@ -39,3 +44,19 @@ def connect_helper(user):
     http = credentials.authorize(http)
     service = build(serviceName='calendar', version='v3', http=http)
     return service
+
+
+def save_event(event, user):
+    if not event and user:
+        return None
+    try:
+        # import pdb; pdb.set_trace()
+        print('e:', event)
+        event_data = Events(user=user, description=event.get('description'),
+                            updated=datetime.strptime(event.get('updated'),
+                                                      "%Y-%m-%dT%H:%M:%S.%fZ"))
+        event_data.save()
+        return event_data
+    except IntegrityError as e:
+        print(e.__cause__)
+        return None
